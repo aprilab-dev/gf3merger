6fdea8f28b3038491dc5d20910ccbdea1956062c
Yuxiao QIN <yuxiao.qin@nwpu.edu.cn>
2023-02-20 14:46:21 +0800

    added more functions for testing purpose.

--- gf3merger/merge.py
+++ gf3merger/merge.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from gf3merger.utils import _read_rslc, _read_res
 
 def cross_interferogram(parent_slc_path:str, child_slc_path:str)->np.ndarray:
@@ -10,4 +11,37 @@ def cross_interferogram(parent_slc_path:str, child_slc_path:str)->np.ndarray:
     parent_slc = _read_rslc(parent_slc_path, lines=lines, samples=samples)
     child_slc = _read_rslc(child_slc_path, lines=lines, samples=samples)
 
+    # check_amplitude(parent_slc, child_slc)
+
     return parent_slc * np.conj(child_slc)
+
+def compensate_phase(cross_interf:np.ndarray):
+    pass
+
+def check_amplitude(parent_slc, child_slc):
+
+    amplitude_diff = np.abs(parent_slc) - np.abs(child_slc)
+    # # generate a plot and save to disk from a numpy array
+    # plt.imshow(amplitude_diff)
+    # plt.savefig("amplitude_difference.png")
+
+    P = np.abs(parent_slc)
+    C = np.abs(child_slc)
+
+    # plt.imshow(np.log(P),vmax=3)
+    # plt.savefig("parent_overlap.png")
+    # plt.imshow(np.log(C),vmax=3)
+    # plt.savefig("child_overlap.png")
+
+    diff_ratio = P/C
+
+    diff_ratio[diff_ratio==np.inf]=0
+    diff_ratio[diff_ratio==-np.inf]=0
+    diff_ratio[np.isnan(diff_ratio)]=0
+
+    print(np.mean(diff_ratio))
+    print(np.std(diff_ratio))
+    plt.imshow(diff_ratio,vmin=0.4, vmax=0.8)
+    plt.colorbar()
+    plt.savefig("diff_ratio.png")
+    plt.clf()
--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,13 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from gf3merger.merge import cross_interferogram
+from gf3merger.utils import _read_rslc, _read_res
+
 
 def test_cross_interferogram():
     parent_slc_path = "/data/tests/yuxiao/gf3_mosaic_test/stack/process/S01B01/20210225"
     child_slc_path = "/data/tests/yuxiao/gf3_mosaic_test/stack/process/S01B01/20210226"
+    master_slc_path = "/data/tests/yuxiao/gf3_mosaic_test/stack/process/S01B01/20210127"
+
+    # cross_interf = cross_interferogram(parent_slc_path, child_slc_path)
+
+    lines  = int(_read_res(parent_slc_path, "Last_line (w.r.t. original_master)"))
+    samples  = int(_read_res(parent_slc_path, "Last_pixel (w.r.t. original_master)"))
+
+    parent_slc = _read_rslc(parent_slc_path, lines=lines, samples=samples)
+    child_slc = _read_rslc(child_slc_path, lines=lines, samples=samples)
+    master_slc = _read_rslc(master_slc_path, lines=lines, samples=samples)
+
+    # plot SLC
+    plt.imshow(np.abs(master_slc), vmax=300)
+    plt.savefig("master_slc.png")
+
+    plt.imshow(np.abs(child_slc), vmax=300)
+    plt.savefig("child_slc.png")
+
+    plt.imshow(np.abs(parent_slc), vmax=300)
+    plt.savefig("parent_slc.png")
+
+    # cross_interf_crop = cross_interf[13000:15500, :]
+    # plt.imshow(np.angle(cross_interf), cmap="rainbow")
+    # plt.savefig("cross_interf.png")
+
+    plt.imshow(np.angle(master_slc*np.conj(child_slc)), vmin=-3.14, vmax=3.14, cmap="rainbow")
+    plt.savefig("interf_w_child.png")
+
+    plt.imshow(np.angle(master_slc*np.conj(parent_slc)),vmin=-3.14, vmax=3.14, cmap="rainbow")
+    plt.savefig("interf_w_parent.png")
+
+    # # generate a plot and save to disk from a numpy array
+    # plt.imshow(np.angle(cross_interf))
+    # plt.savefig("cross_interf.png")
 
 
-    cross_interf = cross_interferogram(parent_slc_path, child_slc_path)
-    # generate a plot and save to disk from a numpy array
-    plt.imshow(np.angle(cross_interf))
-    plt.savefig("cross_interf.png")
