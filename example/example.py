b6441181a2ef9630a09e8072f5e8708d7963dfe6
Yuxiao QIN <hi@aprilab.dev>
2025-09-24 00:54:44 +0800

    updated example for glacier proj.

--- example/example.py
+++ example/example.py
@@ -1,16 +1,10 @@
 from gf3merger import merge
 
 dates_to_be_merged = (
-    ("20201003","20201004"),
-    ("20201101","20201102"),
-    ("20201130","20201201"),
-    ("20210225","20210226"),
-    ("20221027","20221028"),
-    ("20221125","20221126"),
-    ("20230122","20230123"),
+    ("20240208","20240209"),
 )
 
-dir_slc = "fakepath"
+dir_slc = "/data/tests/ruikang/cn_xinjiang_glacier_gf3_test/stack/process/S01B01"
 
 for parent_date, child_date in dates_to_be_merged:
     merge.GF3Merger(dir_slc=dir_slc, parent_date=parent_date, child_date=child_date).merge()
